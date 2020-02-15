bae36dfbd0eba7d054be440b9ca42ef7dc2fd844
acedillo <abraham.cedillo@gmail.com>
2020-02-15 15:42:27 -0500

    added layout files

---
+++ app/src/main/java/com/example/github/CommitFragment.java
@@ -0,0 +1,9 @@
+package com.example.github;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CommitFragment extends Fragment {
+
+    private RecyclerView commitList;
+}
