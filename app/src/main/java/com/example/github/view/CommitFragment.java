757d91b45f0aafd784dc38b4d41b1e06b40c50ad
acedillo <abraham.cedillo@gmail.com>
2020-02-15 16:57:37 -0500

    Added View components

---
+++ app/src/main/java/com/example/github/view/CommitFragment.java
@@ -0,0 +1,9 @@
+package com.example.github.view;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CommitFragment extends Fragment {
+
+    private RecyclerView commitList;
+}
