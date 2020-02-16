42d10b92a5a4b81eb58ffdd861d6cb16a2edf81e
acedillo <abraham.cedillo@gmail.com>
2020-02-16 16:12:44 -0500

    added view model

---
+++ app/src/main/java/com/example/github/viewmodel/GithubViewModel.java
@@ -0,0 +1,53 @@
+package com.example.github.viewmodel;
+
+import com.example.github.model.GithubResponse;
+import com.example.github.repository.CommitRepository;
+import com.example.github.repository.RetrofitCommitRepository;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+public class GithubViewModel extends ViewModel {
+
+    private CommitRepository commitRepository;
+
+    public GithubViewModel(CommitRepository commitRepository){
+        this.commitRepository = commitRepository;
+    }
+
+    private MutableLiveData<Boolean> loading;
+    private MutableLiveData<List<GithubResponse>> response;
+
+    public static ViewModelProvider.Factory getFactory() {
+        return new ViewModelProvider.Factory() {
+            @NonNull
+            @Override
+            public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+                return (T) new GithubViewModel(new RetrofitCommitRepository());
+            }
+        };
+    }
+
+    public LiveData<Boolean> isLoading(){
+        return loading;
+    }
+
+    public LiveData<List<GithubResponse>> getResponse(){
+        return response;
+    }
+
+    void loadCommitList(String owner, String repo){
+        new Thread(() ->{
+            loading.postValue(true);
+            List<GithubResponse> commitList = commitRepository.getCommitList(owner, repo);
+            loading.postValue(false);
+            response.postValue(commitList);
+        });
+    }
+
+}
